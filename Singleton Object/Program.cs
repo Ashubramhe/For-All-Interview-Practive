522d68040f7a61a843d7da21f18f930ad09f211f
Ashubramhe <aashubramhe553@gmail.com>
2023-08-03 12:29:05 +0530

    Singleton  Ocject

---
+++ Singleton Object/Program.cs
@@ -0,0 +1,30 @@
+﻿ public class Singloton
+{
+    private static Singloton _instance;
+
+    private Singloton() 
+    {
+    
+    }
+
+    public static Singloton OBJECT()
+    {
+        if (_instance == null)
+        {
+            _instance = new Singloton();
+        }
+
+        return _instance;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Singloton S1 = Singloton.OBJECT();
+
+        Console.ReadLine();
+    }
+}
