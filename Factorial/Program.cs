811b6cd3b53598b6aa79992571a560b0adf5eeff
Ashubramhe <aashubramhe553@gmail.com>
2023-08-25 10:08:34 +0530

    Add Some Functionality

---
+++ Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please Enter Factorial Number");
+        int FactNum = int.Parse(Console.ReadLine());
+        int FactRest = 1;
+
+        for (int i = 1; i <= FactNum; i++)
+        {
+            Console.WriteLine($"{FactRest = i * FactRest} That Was Flow");
+        }
+        Console.WriteLine($"{0} is {1}",FactNum,FactRest);
+        Console.ReadLine();
+    }
+}
---
+++ SwappingNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int First, Second, Ntemp;
+
+        Console.WriteLine("Please Enter First Number Here");
+        First=int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please Enter Second Number");
+        Second=int.Parse(Console.ReadLine());
+
+        Ntemp = First;
+        First = Second;
+        Second = Ntemp;
+
+        Console.WriteLine($"After Swapping First: {First}  Second: {Second}");
+        Console.ReadLine();
+    }
+}
